b8ff7f6497b0a35da81766beceaf9e2d504b20f1
kitegi <>
2020-09-03 15:53:51 +0200

    clean up benchmark

--- benchmark/pow2.cpp
+++ benchmark/pow2.cpp
@@ -1,5 +1,3 @@
-// c++ -std=c++17 -march=haswell -O3 -Iinclude
-
 #define CXX_MPFR_DEBUG 0
 #include "mpfr/mpfr.hpp"
 
@@ -12,104 +10,35 @@ using bscalar_t = boost::multiprecision::number<
     boost::multiprecision::mpfr_float_backend<N, boost::multiprecision::allocate_stack>,
     boost::multiprecision::et_off>;
 
-void f(scalar_t<100>& a) { a = 128.0; }
+template <typename T, typename Rhs>
+void bench_mul_div(ankerl::nanobench::Bench& bench, Rhs x, std::string const& name) {
+  T a = sqrt(T{2.0});
+  T c{};
 
-auto g(scalar_t<100>& a, scalar_t<100>& b) { return a * b; }
+  ankerl::nanobench::doNotOptimizeAway(&a);
+  ankerl::nanobench::doNotOptimizeAway(&c);
+
+  bench.run("100 digits10: mul 128" + name, [&] {
+    c = a * x;
+    ankerl::nanobench::clobberMemory();
+  });
+  bench.run("100 digits10: div 128" + name, [&] {
+    c = a / x;
+    ankerl::nanobench::clobberMemory();
+  });
+}
 
 auto main() -> int {
 
   auto bench = ankerl::nanobench::Bench();
-  bench.minEpochIterations(100000);
-  {
-    using T = scalar_t<100>;
-    T a = sqrt(T{2.0});
-    T c;
 
-    ankerl::nanobench::detail::doNotOptimizeAway(&a);
-    ankerl::nanobench::detail::doNotOptimizeAway(&c);
+  bench.minEpochTime(std::chrono::nanoseconds{100'000'000ULL});
 
-    bench.run("100 digits10: mul 128", [&] {
-      c = a * 128.0;
-      ankerl::nanobench::clobberMemory();
-    });
-    bench.run("100 digits10: div 128", [&] {
-      c = a / 128.0;
-      ankerl::nanobench::clobberMemory();
-    });
-  }
-  {
-    using T = bscalar_t<100>;
-    T a = sqrt(T{2.0});
-    T c;
+  bench_mul_div<scalar_t<100>>(bench, 128.0, "d");
+  bench_mul_div<bscalar_t<100>>(bench, 128.0, "d boost");
+  bench_mul_div<bscalar_t<100>>(bench, 128, "i boost");
 
-    ankerl::nanobench::detail::doNotOptimizeAway(&a);
-    ankerl::nanobench::detail::doNotOptimizeAway(&c);
-
-    bench.run("100 digits10: boost mul 128", [&] {
-      c = a * 128.0;
-      ankerl::nanobench::clobberMemory();
-    });
-    bench.run("100 digits10: boost div 128", [&] {
-      c = a / 128.0;
-      ankerl::nanobench::clobberMemory();
-    });
-    bench.run("100 digits10: boost mul 128 int", [&] {
-      c = a * 128;
-      ankerl::nanobench::clobberMemory();
-    });
-    bench.run("100 digits10: boost div 128 int", [&] {
-      c = a / 128;
-      ankerl::nanobench::clobberMemory();
-    });
-  }
-
-  bench.run("", [] {});
-  bench.run("", [] {});
-
-  {
-    using T = scalar_t<1000>;
-    T a = sqrt(T{2.0});
-    T c;
-
-    ankerl::nanobench::detail::doNotOptimizeAway(&a);
-    ankerl::nanobench::detail::doNotOptimizeAway(&c);
-
-    bench.run("1000 digits10: mul 128", [&] {
-      c = a * 128.0;
-      ankerl::nanobench::clobberMemory();
-    });
-    bench.run("1000 digits10: div 128", [&] {
-      c = a / 128.0;
-      ankerl::nanobench::clobberMemory();
-    });
-  }
-  {
-    using T = bscalar_t<1000>;
-    T a = sqrt(T{2.0});
-    T c;
-
-    ankerl::nanobench::detail::doNotOptimizeAway(&a);
-    ankerl::nanobench::detail::doNotOptimizeAway(&c);
-
-    bench.run("1000 digits10: boost mul 128", [&] {
-      c = a * 128.0;
-      ankerl::nanobench::clobberMemory();
-    });
-    bench.minEpochIterations(1000)
-        .run(
-            "1000 digits10: boost div 128",
-            [&] {
-              c = a / 128.0;
-              ankerl::nanobench::clobberMemory();
-            })
-        .minEpochIterations(100000);
-    bench.run("1000 digits10: boost mul 128 int", [&] {
-      c = a * 128;
-      ankerl::nanobench::clobberMemory();
-    });
-    bench.run("1000 digits10: boost div 128 int", [&] {
-      c = a / 128;
-      ankerl::nanobench::clobberMemory();
-    });
-  }
+  bench_mul_div<scalar_t<1000>>(bench, 128.0, "d");
+  bench_mul_div<bscalar_t<1000>>(bench, 128.0, "d boost");
+  bench_mul_div<bscalar_t<1000>>(bench, 128, "i boost");
 }
