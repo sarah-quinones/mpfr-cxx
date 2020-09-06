37bb7da979453a90d40f731095d3ab84077fca0a
kitegi <>
2020-09-06 09:18:12 +0200

    remove redundant macro definitions

--- include/mpfr/detail/mpfr.hpp
+++ include/mpfr/detail/mpfr.hpp
@@ -4,18 +4,6 @@
 #include "mpfr/enums.hpp"
 #include "mpfr/detail/prologue.hpp"
 
-#if CXX_MPFR_DEBUG == 1
-#define CXX_MPFR_STRINGIZE2(...) #__VA_ARGS__
-#define CXX_MPFR_STRINGIZE(...) CXX_MPFR_STRINGIZE2(__VA_ARGS__)
-#define CXX_MPFR_ASSERT(...)                                                                       \
-  (static_cast<bool>(__VA_ARGS__)                                                                  \
-       ? (void)0                                                                                   \
-       : ::mpfr::_::crash_with_message("assertion_failed at " __FILE__                             \
-                                       ":" CXX_MPFR_STRINGIZE(__LINE__) "\n" #__VA_ARGS__))
-#else
-#define CXX_MPFR_ASSERT(...) ((void)0)
-#endif
-
 namespace mpfr {
 
 template <precision_t> struct mp_float_t;
